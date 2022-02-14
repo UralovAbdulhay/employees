696c41c4a597812afb2c8a7201d15d9bed10502e
Abdulhay <abdulhayuralov8185603@gmail.com>
2022-02-14 19:47:13 +0500

    second commit

---
+++ src/main/java/com/example/demo/service/impl/AttendanceServiceImpl.java
@@ -0,0 +1,44 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.Attendance;
+import com.example.demo.service.AttendanceService;
+
+import java.util.Collection;
+import java.util.List;
+
+public class AttendanceServiceImpl implements AttendanceService {
+    @Override
+    public Attendance save(Attendance attendance) {
+        return null;
+    }
+
+    @Override
+    public List<Attendance> save(Collection<Attendance> attendances) {
+        return null;
+    }
+
+    @Override
+    public Attendance findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Attendance> findAll(int page, int size) {
+        return null;
+    }
+
+    @Override
+    public Attendance update(Attendance attendance, Long id) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
+
+    @Override
+    public boolean existById(Long id) {
+        return false;
+    }
+}
