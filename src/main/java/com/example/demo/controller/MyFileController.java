3346c46a53e3fa5878f05926837485a718727561
UralovAbdulhay <abdulhayuralov8185603@gmail.com>
2022-02-16 11:34:04 +0500

    ruuuuun

--- src/main/java/com/example/demo/controller/MyFileController.java
+++ src/main/java/com/example/demo/controller/MyFileController.java
@@ -1,11 +1,11 @@
-package com.example.demo.file;
+package com.example.demo.controller;
 
 
 
+import com.example.demo.file.MyFileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,7 +13,7 @@ import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/admin/file")
+@RequestMapping("/file")
 @RequiredArgsConstructor
 @CrossOrigin(maxAge = 36000000)
 public class MyFileController {
@@ -22,14 +22,12 @@ public class MyFileController {
     private final MyFileService myFileService;
 
 
-    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/save-all")
     public ResponseEntity uploadMultipleFiles(@RequestParam("files") List<MultipartFile> files) {
         System.out.println(files);
         return ResponseEntity.ok(myFileService.saveAll(files));
     }
 
-    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/save")
     public ResponseEntity uploadFile(@RequestParam("files") MultipartFile file) {
         System.out.println(file);
