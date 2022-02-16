8841638a38d71334a1491e4144837f3ece2534ed
UralovAbdulhay <abdulhayuralov8185603@gmail.com>
2022-02-16 11:51:54 +0500

    deyarli tayyor

--- src/main/java/com/example/demo/config/SecurityConfiguration.java
+++ src/main/java/com/example/demo/config/SecurityConfiguration.java
@@ -8,40 +8,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-//    private final UserDetailsService userDetailsService;
-//    private final JwtTokenProvider jwtTokenProvider;
-//    private final JwtAuthEntryPoint jwtAuthEntryPoint;
 
-//    public SecurityConfiguration(@Qualifier("customUserDetailes") @Lazy UserDetailsService userDetailsService, JwtTokenProvider jwtTokenProvider, JwtAuthEntryPoint jwtAuthEntryPoint) {
-//        this.userDetailsService = userDetailsService;
-//        this.jwtTokenProvider = jwtTokenProvider;
-//        this.jwtAuthEntryPoint = jwtAuthEntryPoint;
-//    }
-
-    @Bean
-    public AuthenticationManager authenticationManager() throws Exception {
-        return super.authenticationManager();
-    }
-
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth
-//                .userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-//    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+//                .csrf().disable()
                 .cors().disable()
                 .exceptionHandling()
-//                .authenticationEntryPoint(jwtAuthEntryPoint)
                 .and()
                 .headers().frameOptions().and().and()
                 .authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/swagger-ui.html", "/webjars/**", "/swagger-resources", "/swagger-resources/**", "/v2/**", "/csrf").permitAll()
-//                .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .disable();
