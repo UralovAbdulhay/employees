42e5c6cabfcb184feb0b26c08287e6630a0f18b7
UralovAbdulhay <abdulhayuralov8185603@gmail.com>
2022-02-18 15:48:55 +0500

    test going

--- src/main/java/com/example/demo/tests/excel/ExcelImportTest.java
+++ src/main/java/com/example/demo/tests/excel/ExcelImportTest.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gembox.spreadsheet.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ExcelImportTest {
 
@@ -17,31 +19,73 @@ public class ExcelImportTest {
 
         List<Xodim> xodims = new ArrayList<>();
 
-        StringBuilder sb = new StringBuilder();
 
-        // Iterate through all worksheets in an Excel workbook.
-        for (ExcelWorksheet worksheet : workbook.getWorksheets()) {
-            sb.append('\n');
-            sb.append(String.format("%1$s %2$s %1$s", "-----", worksheet.getName()));
-            System.out.println(worksheet.getRows().size());
+        int rowCount = workbook.getWorksheet(0).getRows().size();
 
-            // Iterate through all rows in an Excel worksheet.
-            for (ExcelRow row : worksheet.getRows()) {
-                sb.append('\n');
+        ExcelRowCollection rows = workbook.getWorksheet(0).getRows();
 
-                // Iterate through all allocated cells in an Excel row.
-                for (ExcelCell cell : row.getAllocatedCells()) {
-                    if (cell.getValueType() != CellValueType.NULL)
-                        sb.append(String.format("%1$s [%2$s]           ", cell.getValue(), cell.getValueType()));
-                    else
-                        sb.append("                        ");
+        int headerRowIndex = -1;
+
+        List<HashMap<String, Object>> hashMaps = new ArrayList<>();
+
+        for (int i = 0; i < rows.size(); i++) {
+            ExcelCellCollection cells = rows.get(i).getAllocatedCells();
+            HashMap<String, Object> map = new HashMap<String, Object>();
+
+            for (int j = 0; j < cells.size(); j++) {
+                ExcelCell cell = cells.get(j);
+                if (cell.getValueType() != CellValueType.NULL) {
+                    if (headerRowIndex == -1) {
+                        headerRowIndex = i;
+                    } else {
+                        map.put(String.valueOf(rows.get(headerRowIndex).getCell(j).getValue()), cell.getValue());
+
+                        System.out.print(cell.getValue() + "\t\t");
+                    }
                 }
             }
+
+            if (!map.isEmpty()) {
+                hashMaps.add(map);
+            }
+            System.out.println();
+
+
         }
+
+
+        System.out.println();
+        System.out.println(hashMaps);
+
+        System.out.println(headerRowIndex);
 
         ObjectMapper mapper = new ObjectMapper();
 
-        System.out.println(sb.toString());
+        List<Xodim> xodimList = hashMaps
+                .stream()
+                .skip(1)
+                .map(e -> mapper.convertValue(e, Xodim.class))
+                .collect(Collectors.toList());
+
+
+        System.out.println();
+        System.out.println(xodimList);
+        System.out.println(xodimList.get(1));
+        System.out.println(xodimList.get(1).getAddress());
+
+//        for (ExcelRow row : workbook.getWorksheet(0).getRows()) {
+//
+//
+//            for (ExcelCell cell : row.getAllocatedCells()) {
+//                if (cell.getValueType() != CellValueType.NULL)
+//                    System.out.println((("" + cell.getValue() + cell.getValueType())));
+//            }
+//        }
+//
+//
+//        ObjectMapper mapper = new ObjectMapper();
+
+
     }
 
 
